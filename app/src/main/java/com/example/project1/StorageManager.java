dfb9b6ee1decd38914d5968fe9b15d72c31932ec
Mohammad Haghighat <mhaghighat@rayana.ir>
2019-03-02 17:09:59 +0330

    Added file read/write

--- app/src/main/java/com/example/project1/StorageManager.java
+++ app/src/main/java/com/example/project1/StorageManager.java
@@ -1,16 +1,34 @@
 package com.example.project1;
 
+import android.content.Context;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
 import java.util.ArrayList;
+import java.util.logging.Logger;
 
 public class StorageManager {
-    public void Save(int lastNumber) {
+    public void Save(int lastNumber, Context context) {
         // TODO create new thread named "storage"
-
+        try {
+            OutputStreamWriter outputStreamWriter = new OutputStreamWriter(context.openFileOutput("config.txt", Context.MODE_PRIVATE));
+            outputStreamWriter.write(lastNumber);
+            outputStreamWriter.close();
+        }
+        catch (Exception e) {
+            Log.e("Exception", "File write failed: " + e.toString());
+        }
     }
 
-    public ArrayList<Integer> Load() {
+    public ArrayList<Integer> Load(Context context) {
         // TODO create new thread named "storage"
-        int lastNumber = readLastNumber();
+        int lastNumber = readLastNumber(context);
 
         ArrayList<Integer> result = new ArrayList<>();
         for (int i = 1; i <= 10; i++) {
@@ -20,7 +38,21 @@ public class StorageManager {
         return result;
     }
 
-    private int readLastNumber() {
-        return 10;
+    private int readLastNumber(Context context) {
+        StringBuilder stringBuilder = new StringBuilder();
+        String line;
+        BufferedReader reader = null;
+
+        try {
+            reader = new BufferedReader(new FileReader(new File(context.getFilesDir(), "config.txt")));
+            while ((line = reader.readLine()) != null) stringBuilder.append(line);
+
+        } catch (FileNotFoundException e) {
+            Log.e("Exception", e.toString());
+        } catch (IOException e) {
+            Log.e("Exception", e.toString());
+        }
+
+        return Integer.parseInt(stringBuilder.toString());
     }
 }
